2bd27939e574ab71b5a142ef8c3c5f57220ae730
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-09-18 11:06:49 +0530

    added addEvent route

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const morgan = require("morgan");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
 
-dotenv.config({ path: '.env' });
+// dotenv.config({ path: '.env' });
 
 
 const app = new express();
 
 
 // connecting mongodb
-// require("./db/db")();
+require("./db/db")();
 
 //middlewares
 app.use(morgan('dev'));
@@ -19,7 +19,7 @@ app.use(express.json());
 //routes
 // app.use("/signup",require("./routes/config/signup"));
 // app.use("/login",require("./routes/config/login"));
-// app.use(require("./routes/student_routes"));
+app.use("/addevent",require("./routes/addEvent"));
 // app.use(require("./routes/admin_routes"));
 
-app.listen(3000, console.log(`listening on port ${process.env.PORT}`));
+app.listen(5000, console.log(`listening on port 5000`));
