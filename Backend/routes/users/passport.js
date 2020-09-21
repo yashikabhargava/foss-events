b4caeb8ea6f2b14ef679e15f4551d2d9c4955f36
ayushtiwari57 <ayushtiwari572000@gmail.com>
2020-09-21 21:50:45 +0530

    modified google oauth

--- Backend/routes/users/passport.js
+++ Backend/routes/users/passport.js
@@ -1,23 +1,13 @@
 const passport = require('passport');
 const dotenv = require('dotenv');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-dotenv.config({ path: 'var.env' });
+dotenv.config({ path: '.env' });
 
 passport.serializeUser(function(user, done) {
-  /*
-  From the user take just the id (to minimize the cookie size) and just pass the id of the user
-  to the done callback
-  PS: You dont have to do it like this its just usually done like this
-  */
   done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-  /*
-  Instead of user this function usually recives the id 
-  then you use the id to select the user from the db and pass the user obj to the done callback
-  PS: You can later access this data in any routes in: req.user
-  */
   done(null, user);
 });
 
