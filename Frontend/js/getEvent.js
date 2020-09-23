6fc27d4079846d7e2640797d35ffd81ab976473a
Naman Shankhydhar <naman.geek@gmail.com>
2020-09-23 12:45:36 +0530

    data fetching from events.json file

---
+++ Frontend/js/getEvent.js
@@ -0,0 +1,34 @@
+// Fetch data from events.json file
+fetch("../data/events.json")
+  .then((res) => res.json())
+  .then((res) => {
+    let cardContainer = document.getElementsByClassName("card-container")[0];
+    for (let data in res) {
+      let eventCard = document.createElement("div");
+      eventCard.className = "event_card";
+      let eventTitle = document.createElement("div");
+      eventTitle.className = "event_title";
+      let heading = document.createElement("h3");
+      heading.innerText = res[data].title;
+      eventTitle.appendChild(heading);
+      let eventDetails = document.createElement("div");
+      eventDetails.className = "event_details";
+      let eventLink = document.createElement("a");
+      Object.assign(eventLink, {
+        href: `https://${res[data].website}`,
+        target: "_blank",
+        rel: "noopener noreferrer",
+      });
+      let link = document.createElement("h6");
+      link.innerText = 'Go to website';
+      eventLink.appendChild(link);
+      let date = document.createElement("p");
+      date.innerText = res[data].date;
+      let eventDescription = document.createElement("p");
+      eventDescription.innerHTML = `<b>Description:</b> ${res[data].description}`;
+
+      eventDetails.append(eventLink, date, eventDescription);
+      eventCard.append(eventTitle, eventDetails);
+      cardContainer.appendChild(eventCard);
+    }
+  });
